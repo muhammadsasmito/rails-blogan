03e48e2cdc4e28fbe60d679221d7364509dddb24
muhammadsasmito <muhammad.sasmito.a.w@gmail.com>
2018-01-23 00:59:50 +0700

    change root to post

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # untuk default nya membaca controller#method
-  root to: 'welcome#index'
+  root to: 'posts#index'
 
   # jika kalau pake url
   get 'welcome', to: 'welcome#index'
